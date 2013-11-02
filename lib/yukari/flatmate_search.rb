b6985491c52df2188edafb451ad9f7f733867aca
Andrew Grimm <andrew.j.grimm@gmail.com>
2013-11-02 11:14:36 +1100

    Incorporate sumisu

--- lib/yukari/flatmate_search.rb
+++ lib/yukari/flatmate_search.rb
@@ -1,6 +1,11 @@
 $:.unshift __dir__
 
+# FIXME: Hacky
+$:.unshift File.join(__dir__, '..', '..', '..', 'sumisu', 'lib')
+
 require 'ad'
+require 'frequency_comparer'
+require 'set'
 
 # This already has a description
 class Yukari
@@ -9,7 +14,14 @@ class Yukari
     def initialize(filenames)
       @filenames = filenames
 
+      @frequency_analyzer = create_frequency_analyzer
       @flatmate_evaluations = create_flatmate_evaluations
+    end
+
+    def create_frequency_analyzer
+      australian_frequency_data = FrequencyComparer.australian_frequency_data
+      japanese_frequency_data = FrequencyComparer.japanese_frequency_data
+      FrequencyAnalyzer.new(australian_frequency_data, japanese_frequency_data)
     end
 
     def create_flatmate_evaluations
@@ -17,11 +29,15 @@ class Yukari
     end
 
     def create_flatmate_evaluation(filename)
-      FlatmateEvaluation.new(filename)
+      FlatmateEvaluation.new(filename, @frequency_analyzer)
     end
 
     def matching_files
-      @flatmate_evaluations.map(&:filename)
+      matching_flatmates.map(&:filename)
+    end
+
+    def matching_flatmates
+      @flatmate_evaluations.find_all(&:matching?)
     end
   end
 
@@ -29,11 +45,16 @@ class Yukari
   class FlatmateEvaluation
     attr_reader :filename
 
-    def initialize(filename)
+    def initialize(filename, frequency_analyzer)
       @filename = filename
+      @frequency_analyzer = frequency_analyzer
 
       @ad_parser = create_ad_parser
       @ad = parse_ad
+
+      # This part is still a bit uncertain.
+      # I need to experiment a bit.
+      @matching_word = find_matching_word
     end
 
     def create_ad_parser
@@ -43,6 +64,40 @@ class Yukari
     def parse_ad
       # STDERR.puts "Processing #{@filename.inspect}"
       @ad_parser.parse_ad(@filename)
+    end
+
+    def find_matching_word
+      @ad.words.find(&method(:predominantly_japanese?))
+    end
+
+    def matching?
+      @matching_word
+    end
+
+    IGNORE_WORDS = Set.new(%w{Park Rafael Pedro})
+    HARDWIRED_WORDS = Set.new(%w{Japanese})
+
+    def predominantly_japanese?(word)
+      return false if IGNORE_WORDS.include?(word)
+      return true if HARDWIRED_WORDS.include?(word)
+      @frequency_analyzer.predominantly_japanese?(word)
+    end
+  end
+
+  # Analyze frequency data for this application
+  # In particular, try to work out if a word suggets the flatmate
+  # may be Japanese-speaking.
+  class FrequencyAnalyzer
+    def initialize(australian_frequency_data, japanese_frequency_data)
+      @australian_frequency_data = australian_frequency_data
+      @japanese_frequency_data = japanese_frequency_data
+    end
+
+    def predominantly_japanese?(word)
+      australian_frequency = @australian_frequency_data.frequency_for(word)
+      japanese_frequency = @japanese_frequency_data.frequency_for(word)
+      difference = japanese_frequency - australian_frequency
+      difference > 10
     end
   end
 end
