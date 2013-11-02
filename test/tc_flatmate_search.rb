2463e6aae6936f4f562a34e52119eb9148569810
Andrew Grimm <andrew.j.grimm@gmail.com>
2013-11-02 12:28:25 +1100

    Refactor

--- test/tc_flatmate_search.rb
+++ test/tc_flatmate_search.rb
@@ -5,10 +5,14 @@ require 'flatmate_search'
 
 # Test for flatmate search.
 class TestFlatmateSearch < Test::Unit::TestCase
-  def test_flatmate_search
+  def create_flatmate_search
     ad_filename = 'test/data/exclude/ads/page_1.html'
     filenames = [ad_filename]
-    flatmate_search = Yukari::FlatmateSearch.new(filenames)
+    Yukari::FlatmateSearch.new(filenames)
+  end
+
+  def test_flatmate_search
+    flatmate_search = create_flatmate_search
     failure_message = 'Something does not work'
     assert_equal 1, flatmate_search.matching_files.length, failure_message
   end
