6a3ba21d963acf3cb818534e339b86eda8cc1fa5
Andrew Grimm <andrew.j.grimm@gmail.com>
2013-11-10 19:56:39 +1100

    Add executable for ad downloading

--- lib/yukari.rb
+++ lib/yukari.rb
@@ -1,6 +1,7 @@
 $:.unshift File.join(__dir__, 'yukari')
 
 require 'flatmate_search'
+require 'results'
 
 # Parser for Gumtree, to find a Japanese-speaking flatmate.
 class Yukari
@@ -10,5 +11,13 @@ class Yukari
     filenames = Dir.glob(AD_PAGE_GLOB)
     flatmate_search = FlatmateSearch.new(filenames)
     puts flatmate_search.match_report_output
+  end
+
+  def self.download_new_ads!
+    fail 'bin/ad_downloader results_page.html' if ARGV.empty?
+    results_filename = ARGV.first
+    result_parser = ResultParser.new
+    result = result_parser.parse_result(results_filename)
+    result.download_new_ads!
   end
 end
